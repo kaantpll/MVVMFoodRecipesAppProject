dbbd0b683b6958798b91d03521468eb6f454a5ce
kaantpll <kaant43@gmail.com>
2021-05-13 13:26:48 +0300

    Design

---
+++ app/src/main/java/com/example/mvvmfoodrecipesappproject/constans/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.mvvmfoodrecipesappproject.constans
+
+class Constants {
+
+    companion object {
+        const val BASE_URL = "https://api.spoonacular.com"
+        const val API_KEY = "b9a0787b77334430927fd7e467a9ceac"
+    }
+}
---
+++ app/src/main/java/com/example/mvvmfoodrecipesappproject/data/Repository.kt
@@ -0,0 +1,4 @@
+package com.example.mvvmfoodrecipesappproject.data
+
+class Repository {
+}
