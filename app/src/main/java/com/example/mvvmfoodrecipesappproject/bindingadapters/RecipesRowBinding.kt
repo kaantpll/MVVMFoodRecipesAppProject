29b2f9b8e22c010a9dae6ef460c7345feddebf7e
kaantpll <kaant43@gmail.com>
2021-05-21 11:58:06 +0300

    Details Activity

--- app/src/main/java/com/example/mvvmfoodrecipesappproject/bindingadapters/RecipesRowBinding.kt
+++ app/src/main/java/com/example/mvvmfoodrecipesappproject/bindingadapters/RecipesRowBinding.kt
@@ -1,16 +1,38 @@
 package com.example.mvvmfoodrecipesappproject.bindingadapters
 
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
+import androidx.navigation.findNavController
 import coil.load
 import com.example.mvvmfoodrecipesappproject.R
+import com.example.mvvmfoodrecipesappproject.model.Result
+import com.example.mvvmfoodrecipesappproject.ui.fragments.recipes.RecipesFragmentArgs
+import com.example.mvvmfoodrecipesappproject.ui.fragments.recipes.RecipesFragmentDirections
 
 class RecipesRowBinding {
 
     companion object{
+
+        @BindingAdapter("onRecipeClickListener")
+        @JvmStatic
+        fun onRecipeClickListener(recipeRowLayout : ConstraintLayout , result : Result){
+            recipeRowLayout.setOnClickListener {
+
+                try {
+                    val action = RecipesFragmentDirections.actionRecipesFragmentToDetailsActivity(result)
+                    recipeRowLayout.findNavController().navigate(action)
+                }catch (e : Exception){
+                    Log.d("onRecipesClickListener",e.toString())
+
+                }
+            }
+        }
+
 
         @BindingAdapter("loadImageFromUrl")
         @JvmStatic
